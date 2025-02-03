d8df753f5629f79203e799d7365ba505da0b0adf
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2025-02-03 13:07:43 +0530

    corrected assignment stdin stdout

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -7,7 +7,11 @@ public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
